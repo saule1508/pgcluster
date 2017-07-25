dde85d4289324f2eddf937a250a5b7e13cf8dc3d
Pierre Timmermans <ptim007@yahoo.com>
2017-07-25 20:39:39 +0200

    some fixes

--- ansible/roles/cluster/files/start_pgcluster.sh
+++ ansible/roles/cluster/files/start_pgcluster.sh
@@ -7,7 +7,6 @@ set -o allexport
 source /etc/pgcluster/pgcluster.conf
 set +o allexport
 
-NETWORK_NAME="phoenix_network"
-export HOST_GATEWAY_IP=$(docker network inspect $NETWORK_NAME | jq -r ".[0].IPAM.Config[0].Gateway" | grep -m 1 -o "[0-9.]*" | head -n1)
+NETWORK_NAME="pgcluster_network"
 export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -1)
-docker stack deploy -c /opt/pgcluster/docker-compose.yml phoenix
+docker stack deploy -c /opt/pgcluster/docker-compose.yml pgcluster
