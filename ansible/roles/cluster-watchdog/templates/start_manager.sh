06510e81257c5c75f5f9c996d73a847e43de3255
Pierre Timmermans <ptim007@yahoo.com>
2017-12-03 11:40:06 +0100

    missing backslash

--- ansible/roles/cluster-watchdog/templates/start_manager.sh
+++ ansible/roles/cluster-watchdog/templates/start_manager.sh
@@ -9,8 +9,9 @@ set +o allexport
 docker run -d \
   --privileged
   --net=host \
+  -v /var/run/docker:/var/run/docker \
   -p 8080:8080 \
-  -e PG_BACKEND_NODE_LIST=${PG_BACKEND_NODE_LIST}
+  -e PG_BACKEND_NODE_LIST=${PG_BACKEND_NODE_LIST} \
   -e REPMGRDPWD=${REPMGRDPWD:-rep123} \
   --restart=always \
   --name=manager \
