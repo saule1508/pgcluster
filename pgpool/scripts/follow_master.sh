e8cac815aa19329ce3c3544e57faf28262b4bcfc
Pierre Timmermans <ptim007@yahoo.com>
2018-02-15 21:47:42 +0100

    follow master must do attach node after

--- pgpool/scripts/follow_master.sh
+++ pgpool/scripts/follow_master.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #put keys on the remote AND on the local server
-#NOT TESTED
 
 if [ ! -d /var/log/pg ] ; then
  sudo mkdir -p /var/log/pg
@@ -22,7 +21,6 @@ OLD_MASTER_ID=$6
 OLD_PRIMARY_ID=$7
 PGDATA=${PGDATA:-/u01/pg10/data}
 
-
 (
 echo NODEID=${NODEID} 
 echo HOSTNAME=${HOSTNAME}
@@ -35,12 +33,13 @@ echo PGDATA=${PGDATA}
 if [ $NODEID -eq $OLD_PRIMARY_ID ] ; then
   echo "Do nothing as this is the failed master. We could prevent failed master to restart here, so that we can investigate the issue" | tee -a $LOGFILE
 else
-  ssh_options="ssh -p 222 -n -T -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-  set -x
-  $ssh_options postgres@${HOSTNAME} "/usr/pgsql-10/bin/repmgr --log-to-file -f /etc/repmgr/10/repmgr.conf -h ${NEW_MASTER_HOST} -D ${PGDATA} -U repmgr -d repmgr standby follow -v "
-  echo "Sleep 5"
-  sleep 5
+  ssh_options="ssh -p 222 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+  #set -x
+  $ssh_options postgres@${HOSTNAME} "/usr/pgsql-10/bin/repmgr --log-to-file -f /etc/repmgr/10/repmgr.conf -h ${NEW_MASTER_HOST} -D ${PGDATA} -U repmgr -d repmgr standby follow -v"
+  echo "Sleep 10"
+  sleep 10
   echo "Attach node ${NODEID}"
   pcp_attach_node -h localhost -p 9898 -w ${NODEID}
 fi
 ) 2>&1 | tee -a $LOGFILE
+echo "Done follow_master.sh at `date`"  | tee -a $LOGFILE
