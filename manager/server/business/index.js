a2de72dc186fcd29405897cf94287041bdca31cb
Pierre Timmermans <ptim007@yahoo.com>
2018-02-18 18:25:27 +0100

    adding watchdog

--- manager/server/business/index.js
+++ manager/server/business/index.js
@@ -1,18 +1,103 @@
-const request = require('request-promise');
+const bus_health = service => {
+  const request = require("request-promise");
+  return request
+    .get({ uri: "http://" + service + ":8080/health", json: true })
+    .then(response => {
+      console.log("in then");
+      return response;
+    })
+    .catch(error => {
+      console.log("in catch");
+      throw error;
+    });
+};
 
-const bus_health = ( service ) => {
-  return request.get({uri: 'http://' + service + ':8080/health', json: true})
-     .then((response)=>{
-       console.log('in then');
-       return response;
-     })
-     .catch((error)=>{
-       console.log('in catch');
-       throw error;
-     })
+const formatPgpoolWD = arr => {
+  let result = {};
+  let currentItem;
+  let nodeIndex = -1;
+  let nodeArray = [];
+  arr.forEach(el => {
+    if (el.startsWith("Watchdog Cluster Information")) {
+      currentItem = "wd_cluster_info";
+    }
+    if (el.startsWith("Watchdog Node Information")) {
+      currentItem = "wd_node_info";
+    }
+    if (currentItem === "wd_node_info" && el.startsWith("Node Name")) {
+      nodeIndex++;
+      nodeArray.push({ idx: nodeIndex });
+    }
+    line = el.split(":");
+    if (line.length !== 2) {
+      return;
+    }
+    let k = line[0]
+      .trim()
+      .toLowerCase()
+      .replace(/ /g, "_");
+    if (currentItem === "wd_node_info") {
+      nodeArray[nodeIndex][k] = line[1].trim().replace(",", "");
+    } else {
+      result[k] = line[1].trim().replace(",", "");
+    }
+  });
+  result["nodes"] = nodeArray;
+  return result;
+};
+
+const getPgpoolWDStatusFromDB = dbhost => {
+  const { spawn } = require("child_process");
+
+  //  const pools = require('../config/pgpool').pools;
+  //  const getFilesForHost = require('./utils.js').getFilesForHost;
+  //  const directory = '/u02/backup';
+  return new Promise((resolve, reject) => {
+    let response = "";
+    const cmdArgs = [
+      "-p",
+      "222",
+      "-o",
+      "StrictHostKeyChecking=no",
+      "-o",
+      "UserKnownHostsFile=/dev/null",
+      `postgres@${dbhost}`,
+      "-C",
+      "/usr/pgpool-10/bin/pcp_watchdog_info",
+      "-h",
+      "pgpool",
+      "-p",
+      "9898",
+      "-w",
+      "-v"
+    ];
+    const shell = spawn("ssh", cmdArgs);
+    shell.stdout.on("data", data => {
+      response = `${response}${data.toString()}`;
+    });
+    shell.stderr.on("data", data => {
+      console.log(`got error ${data.toString()}`);
+    });
+    shell.on("close", code => {
+      console.log(`shell exited with code ${code}`);
+      if (code === 0) {
+        resolve(formatPgpoolWD(response.split("\n")));
+      } else {
+        reject({ error: code });
+      }
+    });
+    shell.on("error", error => {
+      console.log(`spawn error ${error}`);
+    });
+  });
+};
+/*
+module.exports = {
+  getPgpoolWDStatus: getBackups
 }
-
+*/
 
 module.exports = {
-  'bus_health': bus_health
-}
+  bus_health: bus_health,
+  getPgpoolWDStatusFromDB: getPgpoolWDStatusFromDB
+};
--- manager/server/routes/postgres.js
+++ manager/server/routes/postgres.js
@@ -1,83 +1,108 @@
-var express = require('express')
+var express = require("express");
 var router = express.Router();
-let pg = require('../DAO/pg.js');
+let pg = require("../DAO/pg.js");
 
-router.get('/repl_nodes', function (req, res) {
-	
-	pg.getReplNodes()
-		.then((data)=>{
-			return res.status(200).send(data);
-		})
-		.catch((err)=>{
-			console.log(err);
-			res.status(500).send(err);
-		})
-	;
-	
-})
-
-router.get('/stat_activity',(req,res)=>{
-
-	pg.getStatActivity()
-		.then((data)=>{
-			return res.status(200).send(data);
-		})
-		.catch((err)=>{
-			res.status(500).send(err);
-		})
-	;
+router.get("/repl_nodes", function(req, res) {
+  pg
+    .getReplNodes()
+    .then(data => {
+      return res.status(200).send(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(err);
+    });
 });
 
-router.get('/pool_nodes', (req, res) => {
-	pg.getPoolNodes()
-		.then((data)=>{
-			return res.status(200).send(data);
-		})
-		.catch((err)=>{
-			res.status(500).send(err);
-		})
-	;  
-})
+router.get("/stat_activity", (req, res) => {
+  pg
+    .getStatActivity()
+    .then(data => {
+      return res.status(200).send(data);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
 
-router.get('/dbstates', (req,res)=>{
-   pg.dbStates()
-      .then((data)=>{
-        return res.status(200).send({'result': data, 'timestamp': new Date()});    
-      })
-      .catch((err)=>{
-        console.log(err);
-        let msg = err.detail ? err.detail : null;
-        if (! msg){
-          msg = 'server error ' + (err.code ? err.code + ' - ' : ' - ' ) + (err.errno ? err.errno : '');
+router.get("/pool_nodes", (req, res) => {
+  pg
+    .getPoolNodes()
+    .then(data => {
+      return res.status(200).send(data);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
+
+router.get("/dbstates", (req, res) => {
+  pg
+    .dbStates()
+    .then(data => {
+      return res.status(200).send({ result: data, timestamp: new Date() });
+    })
+    .catch(err => {
+      console.log(err);
+      let msg = err.detail ? err.detail : null;
+      if (!msg) {
+        msg =
+          "server error " +
+          (err.code ? err.code + " - " : " - ") +
+          (err.errno ? err.errno : "");
+      }
+      res.status(500).send({ message: msg, error: err });
+    });
+});
+
+router.get("/replication_stats", (req, res) => {
+  pg
+    .replicationStats()
+    .then(data => {
+      return res.status(200).send(data);
+    })
+    .catch(err => {
+      console.log("error from replicationStats");
+      console.log(err);
+      res.status(500).send(err);
+    });
+});
+
+router.get("/backups", (req, res) => {
+  const getBackups = require("../business/backup.js").getBackups;
+  getBackups()
+    .then(result => {
+      return res.status(200).send(result);
+    })
+    .catch(error => {
+      return res.status(501).send(error);
+    });
+});
+
+router.get("/pgp_watchdog", (req, res) => {
+  const dblist = require("../config/config.js").pg;
+  const getPgpoolWDStatusFromDB = require("../business/index")
+    .getPgpoolWDStatusFromDB;
+  let result;
+  let response;
+  let done = 0;
+
+  dblist.some((el, idx) => {
+    console.log("doing " + el.host);
+    result = getPgpoolWDStatusFromDB(el.host)
+      .then(data => {
+        console.log("then for " + el.host);
+        if (!done) {
+          done++;
+          return res.status(200).send(data);
         }
-        res.status(500).send({'message': msg,'error': err } );
       })
-
-})
-
-router.get('/replication_stats', (req, res) => {
-	pg.replicationStats()
-		.then((data)=>{
-			return res.status(200).send(data);
-		})
-		.catch((err)=>{
-			console.log('error from replicationStats');
-			console.log(err);
-			res.status(500).send(err);
-		})
-	;  
-})
-
-router.get('/backups', (req, res) => {
-  const getBackups = require('../business/backup.js').getBackups;
-  getBackups().then((result)=>{
-    return res.status(200).send(result);
-  })
-  .catch((error)=>{
-    return res.status(501).send(error);
+      .catch(error => {
+        if (done === dblist.length) {
+          return res.status(500).send(error);
+        }
+      });
   });
-})
+});
 
-
-
-module.exports = router
+module.exports = router;
