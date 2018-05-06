9f58f0965ba66a25dfd2282eff3be9f72860ab30
Pierre Timmermans <ptim007@yahoo.com>
2018-05-06 10:44:13 +0200

    fixed .bashrc and get_master

--- postgres/get_master.sh
+++ postgres/get_master.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-str=`psql -U repmgr repmgr -t -c "select name from repl_nodes where type='master' and active='t';"`
+str=`psql -U repmgr repmgr -t -c "select name from nodes where type='primary' and active='t';"`
 echo $str
