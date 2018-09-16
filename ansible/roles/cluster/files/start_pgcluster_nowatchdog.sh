d5f50d4b9f32744db7d16a4ce8c71645ea55d0aa
Pierre Timmermans <ptim007@yahoo.com>
2018-09-16 23:04:28 +0200

    deploy docker swarm with pgpool watchdog

--- ansible/roles/cluster/files/start_pgcluster_nowatchdog.sh
+++ ansible/roles/cluster/files/start_pgcluster_nowatchdog.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# this should start the stack
+# this should start the stack without the watchdog mode
 
 #TODO: check if we are on manager node (stack deploy is not allowed on worker node
 set -o allexport
 source /etc/pgcluster/pgcluster.conf
-source /etc/pgwatchdog/pgwatchdog.conf
 set +o allexport
 
 NETWORK_NAME="pgcluster_network"
 export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -1)
-docker stack deploy -c /opt/pgcluster/docker-compose.yml pgcluster
+docker stack deploy -c /opt/pgcluster/docker-compose-nowatchdog.yml pgcluster
