398acf73db94a00dc965867be97c225c192c9ec2
PTI <p.timmermans@evs.com>
2019-02-28 11:05:54 +0100

    upgrade to pgpool 4

--- build.sh
+++ build.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-DOCKER_REGISTRY=192.168.1.57:5000
+# unset following variable if you don't need a registry
+#DOCKER_REGISTRY=192.168.1.57:5000
+unset DOCKER_REGISTRY
 VER=`cat version.txt`
 
 # remove volumes so we start from scratch
 docker volume ls | grep pgcluster | awk '{print $2}' | xargs docker volume rm
 docker build -t pg:${VER} --no-cache=false -f postgres/Dockerfile ./postgres
-if [ $? -eq 0 ] ; then
+if [ $? -eq 0 ] && [ ! -z $DOCKER_REGISTRY ] ; then
  echo pushing to local registry
  docker tag pg:$VER $DOCKER_REGISTRY/pg:$VER
  docker push $DOCKER_REGISTRY/pg:$VER
 fi
 docker build -t pgpool:${VER} -f pgpool/Dockerfile ./pgpool
-if [ $? -eq 0 ] ; then
+if [ $? -eq 0 ] && [ ! -z $DOCKER_REGISTRY ] ; then
  echo pushing to local registry
  docker tag pgpool:$VER $DOCKER_REGISTRY/pgpool:$VER
  docker push $DOCKER_REGISTRY/pgpool:$VER
@@ -21,6 +23,6 @@ thisdir=$(pwd)
 cd manager/build
 ./build.bash
 cd $thisdir
-cd test_services/nodejs-dbinserter/
-./build.bash
-cd $thisdir
+#cd test_services/nodejs-dbinserter/
+#./build.bash
+#cd $thisdir
