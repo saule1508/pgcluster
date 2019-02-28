6c185977275eba855ad0859316d8e60d3240b0f7
PTI <p.timmermans@evs.com>
2019-02-28 10:03:29 +0100

    upgrade pgpool to version 4

--- postgres/scripts/start_repmgr.sh
+++ postgres/scripts/start_repmgr.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ "$INITIAL_NODE_TYPE" == "single" -o "a${REPMGRD} == "afalse" -o "a${REPMGRD} == "ano" ] ; then
+if [ "$INITIAL_NODE_TYPE" == "single" -o "a${REPMGRD}" == "afalse" -o "a${REPMGRD}" == "ano" ] ; then
   echo "single node type is $INITIAL_NODE_TYPE or REPMGRD is ${REPMGRD}: do not start repmgr"
-  tail -f /scripts/start_repmgr.sh
+  exec tail -f /scripts/start_repmgr.sh
 else
   echo "sleep 60 before starting repmgrd"
   sleep 60
-  exec /usr/pgsql-10/bin/repmgrd -f /etc/repmgr/10/repmgr.conf --verbose --monitoring-history
+  exec /usr/pgsql-10/bin/repmgrd -f /etc/repmgr/10/repmgr.conf --verbose --monitoring-history --daemonize=false
 fi
