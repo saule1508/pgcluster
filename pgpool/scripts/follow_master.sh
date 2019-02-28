6f8a21e145c69a11e68122de68c92c3edef51f33
Pierre Timmermans <p.timmermans@evs.com>
2019-02-28 23:55:27 +0100

    protect againt degenerated master

--- pgpool/scripts/follow_master.sh
+++ pgpool/scripts/follow_master.sh
@@ -30,11 +30,17 @@ echo NEW_MASTER_HOST=${NEW_MASTER_HOST}
 echo OLD_MASTER_ID=${OLD_MASTER_ID}
 echo OLD_PRIMARY_ID=${OLD_PRIMARY_ID}
 echo PGDATA=${PGDATA}
+pcp_node_info -h localhost -p 9898 -w $NODEID | tee -a $LOGFILE
 if [ $NODEID -eq $OLD_PRIMARY_ID ] ; then
   echo "Do nothing as this is the failed master. We could prevent failed master to restart here, so that we can investigate the issue" | tee -a $LOGFILE
 else
   ssh_options="ssh -p 222 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
   #set -x
+  in_reco=$( $ssh_options postgres@${HOSTNAME} 'psql -t -c "select pg_is_in_recovery();"' | head -1 | awk '{print $1}' )
+  if [ "a${in_reco}" != "a" ] ; then
+    echo "Node $HOSTNAME is not in recovery, probably a degenerated master, skip it" | tee -a $LOGFILE
+    exit 0
+  fi
   $ssh_options postgres@${HOSTNAME} "/usr/pgsql-10/bin/repmgr --log-to-file -f /etc/repmgr/10/repmgr.conf -h ${NEW_MASTER_HOST} -D ${PGDATA} -U repmgr -d repmgr standby follow -v"
   echo "Sleep 10"
   sleep 10
